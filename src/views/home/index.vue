92f984e3a26f9a13e146df18c10c2c7502f7b01f
宋佳 <1370402406@qq.com>
2021-02-27 00:03:22 +0800

    01-初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home-container">
+    <!-- 头部导航菜单 -->
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+    >
+      <el-menu-item index="1">处理中心</el-menu-item>
+      <el-menu-item index="2">处理中心</el-menu-item>
+      <el-menu-item index="3">处理中心</el-menu-item>
+      <el-menu-item index="4">处理中心</el-menu-item>
+    </el-menu>
+    <!-- 子路由出口 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homePage',
+  components: {},
+  props: {},
+  data() {
+    return {
+      activeIndex: 1
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
---
+++ src/views/user/register.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="register-container">注册</div>
+</template>
+
+<script>
+export default {
+  name: 'registerPage',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
